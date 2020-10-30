395326f7d908ab8959c331aabcda22f0a01bbd58
Arnold <antaloczi.arnold@gmail.com>
2020-10-30 19:34:46 +0100

    Initial commit

---
+++ js/gallery.js
@@ -0,0 +1,18 @@
+let imageContainer = document.querySelector(".image-container");
+
+function photoBoxGenerator(amount) {
+    for (let i = 0; i < amount; i++) {
+        let photoBoxDiv = document.createElement("div");
+        photoBoxDiv.classList.add("image-box");
+
+        let randomPhoto = document.createElement("img");
+        randomPhoto.setAttribute("src", `https://source.unsplash.com/800x600/?computer,programming${i}`);
+        randomPhoto.setAttribute("alt", "Számítógép vagy programozós kép");
+        randomPhoto.setAttribute("width", "400px");
+
+        photoBoxDiv.appendChild(randomPhoto);
+        imageContainer.appendChild(photoBoxDiv);
+    }
+}
+
+photoBoxGenerator(20);
